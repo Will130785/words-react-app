dbf70ecb4a2bbd8fca4018cfc7906118b8fb040d
Will Constable <will_constable@msn.com>
2025-09-07 17:05:43 +0100

    add envs

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,8 @@
+const { VITE_API_URL } = import.meta.env
+
 export const getWords = async () => {
   try {
-    const res = await fetch(`http://localhost:4000/get-words`)
+    const res = await fetch(`${VITE_API_URL}/get-words`)
     if (!res.ok) {
       throw new Error('Error getting words')
     }
@@ -13,7 +15,7 @@ export const getWords = async () => {
 
 export const getWord = async (id: string) => {
   try {
-    const res = await fetch(`http://localhost:4000/get-word/${id}`)
+    const res = await fetch(`${VITE_API_URL}/get-word/${id}`)
     if (!res.ok) {
       throw new Error('Error getting word')
     }
@@ -29,7 +31,7 @@ export const addWord = async (payload: {
   italian: string
 }) => {
   try {
-    const res = await fetch(`http://localhost:4000/add-word`, {
+    const res = await fetch(`${VITE_API_URL}/add-word`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -53,7 +55,7 @@ export const editWord = async (
   }
 ) => {
   try {
-    const res = await fetch(`http://localhost:4000/edit-word/${id}`, {
+    const res = await fetch(`${VITE_API_URL}/edit-word/${id}`, {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
@@ -71,7 +73,7 @@ export const editWord = async (
 
 export const deleteWord = async (id: string) => {
   try {
-    const res = await fetch(`http://localhost:4000/delete-word/${id}`, {
+    const res = await fetch(`${VITE_API_URL}/delete-word/${id}`, {
       method: 'DELETE',
     })
     if (!res.ok) {
